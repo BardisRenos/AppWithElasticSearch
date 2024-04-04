32ede1c3971d21d1563620e187754cc56bc4a4b5
Renos Bardis <renosbardis@gmail.com>
2024-04-04 21:09:53 +0300

    feature: Adding first parts of the application

---
+++ demo/src/main/java/com/app/demo/model/Employee.java
@@ -0,0 +1,18 @@
+package com.app.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Getter
+@Setter
+@Document(indexName = "employee")
+public class Employee {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String department;
+}
